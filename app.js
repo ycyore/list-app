ef934187929e58d0c4d8eb21a1dde9d397d757f4
risaishide <risa@tippsysake.com>
2022-06-30 16:30:04 -0700

    added and modified top.ejs, installed ejs

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/static', express.static('public'))
+app.get('/top', (req, res) => {
+  res.render('top.ejs');
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
